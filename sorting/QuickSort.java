99ecc6cc697da6158a1926c228b22be689809231
Shubham Kaushik <skaushik241000@gmail.com>
2026-08-07 19:44:41 +0530

    Bug fix

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -1,62 +1,62 @@
 package sorting;
 
-// TC - O(NLog(N)), SC- O(1)
-// 1. Pick a pivot and place it at correct position in sorted array ( pivot can first, last, median or random element of the array)
-// 2. Smaller on the left and larger on the right
+// Average TC - O(N log N), Worst TC - O(N²)
+// Average SC - O(log N) due to recursion stack
 public class QuickSort {
 
-    private static int pivotize(int[] arr, int pivot, int low, int high) {
-        int i = low, j = high;
+    private static int partition(int[] arr, int pivot, int low, int high) {
+        int pivotValue = arr[pivot];
+        int i = low;
+        int j = high;
 
-        while(i <= j) {
+        while (i < j) {
 
-            while(arr[i] <= arr[pivot]) {
+            while (i <= high && arr[i] <= pivotValue) {
                 i++;
             }
 
-            while(arr[j] >= arr[pivot]){
+            while (j >= low && arr[j] > pivotValue) {
                 j--;
             }
 
-            if(i <= j) {
+            if (i < j) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
 
-        int temp = arr[j];
-        arr[j] = arr[pivot];
-        arr[pivot] = temp;
+        int temp = arr[pivot];
+        arr[pivot] = arr[j];
+        arr[j] = temp;
 
         return j;
-
     }
 
     private static void quickSort(int[] arr, int low, int high) {
-        if( low >= high ) return;
+        if (low >= high)
+            return;
 
         int pivot = low;
-        int partitionIndex = pivotize(arr, pivot, low, high);
+        int partitionIndex = partition(arr, pivot, low, high);
+
         quickSort(arr, low, partitionIndex - 1);
         quickSort(arr, partitionIndex + 1, high);
-
     }
-
 
     public static void main(String[] args) {
-        int[] arr = new int[]{123,32,54,1,5243,655,6};
-        int low = 0, high = arr.length - 1;
-        System.out.println("Before Sort:-");
-        for(var el : arr) {
+        int[] arr = {123, 32, 54, 1, 5243, 655, 6};
+
+        System.out.println("Before Sort:");
+        for (int el : arr) {
             System.out.print(el + " ");
         }
-        System.out.println("--------------------------");
-        quickSort(arr, low, high);
-        System.out.println("After Sort:-");
-        for(var el : arr) {
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("\nAfter Sort:");
+        for (int el : arr) {
             System.out.print(el + " ");
         }
     }
-
 }
